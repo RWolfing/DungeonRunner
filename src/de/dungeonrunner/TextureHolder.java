cc700438534eaf914a20853931877f9fe5038e0d
Robert Wolfinger <rwolfinger@cellular.de>
2015-10-08 01:03:39 +0200

    Loading and drawing tmx map

--- src/de/dungeonrunner/TextureHolder.java
+++ src/de/dungeonrunner/TextureHolder.java
@@ -1,10 +1,17 @@
 package de.dungeonrunner;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
+import org.jsfml.graphics.Image;
 import org.jsfml.graphics.Texture;
+import org.jsfml.graphics.TextureCreationException;
+
+import tiled.core.Map;
+import tiled.core.Tile;
+import tiled.core.TileSet;
 
 public class TextureHolder {
 
@@ -16,13 +23,13 @@ public class TextureHolder {
 	private HashMap<TextureID, Texture> mTextures;
 	private HashMap<Integer, Texture> mTileTextures;
 
-	public static TextureHolder getInstance(){
-		if(mInstance == null){
+	public static TextureHolder getInstance() {
+		if (mInstance == null) {
 			mInstance = new TextureHolder();
 		}
 		return mInstance;
 	}
-	
+
 	private TextureHolder() {
 		mTextures = new HashMap<>();
 		mTileTextures = new HashMap<>();
@@ -40,21 +47,41 @@ public class TextureHolder {
 		mTextures.put(id, texture);
 		return true;
 	}
-	
-	public boolean loadTileTexture(int textureId, Texture texture){
-		if(texture == null){
-			System.out.println("Loading null texture into TextureHolder");
+
+	private boolean loadImageTexture(int textureId, java.awt.Image awtImage) {
+		try {
+			Image image = new Image();
+			image.create((BufferedImage) awtImage);
+			Texture texture = new Texture();
+			texture.loadFromImage(image);
+			mTileTextures.put(textureId, texture);
+		} catch (TextureCreationException e) {
+			System.err.println("Could not create texture from image");
+			e.printStackTrace();
 			return false;
 		}
-		mTileTextures.put(textureId, texture);
+
 		return true;
+	}
+	
+	public void loadTiledTextures(Map tiledMap) {
+		final int numberTileSets = tiledMap.getTileSets().size();
+
+		for (int i = 0; i < numberTileSets; i++) {
+			TileSet tileSet = tiledMap.getTileSets().get(i);
+			final int numberTiles = tileSet.size();
+			for (int o = 0; o < numberTiles; o++) {
+				Tile tile = tileSet.getTile(o);
+				loadImageTexture(tile.getId(), tile.getImage());
+			}
+		}
 	}
 
 	public Texture getTexture(TextureID id) {
 		return mTextures.get(id);
 	}
-	
-	public Texture getTileTexture(int id){
+
+	public Texture getTileTexture(int id) {
 		return mTileTextures.get(id);
 	}
 }
--- src/de/dungeonrunner/TmxMapLoader.java
+++ src/de/dungeonrunner/TmxMapLoader.java
@@ -5,23 +5,23 @@ import tiled.io.TMXMapReader;
 
 public class TmxMapLoader {
 
-    public static Map loadMap(String filepath){
-    	String fileToOpen = filepath;
+	public static Map loadMap(String filepath) {
+		String fileToOpen = filepath;
 
-        if (fileToOpen == null) {
-            return null;
-        }
+		if (fileToOpen == null) {
+			return null;
+		}
 
-        Map map;
-        try {
-            TMXMapReader mapReader = new TMXMapReader();
-            map = mapReader.readMap(fileToOpen);
-        } catch (Exception e) {
-            System.out.println("Error while reading the map:\n" + e.getMessage());
-            return null;
-        }
+		Map map;
+		try {
+			TMXMapReader mapReader = new TMXMapReader();
+			map = mapReader.readMap(fileToOpen);
+		} catch (Exception e) {
+			System.out.println("Error while reading the map:\n" + e.getMessage());
+			return null;
+		}
 
-        System.out.println(map.toString() + " loaded");
-        return map;
-    }
+		System.out.println(map.toString() + " loaded");
+		return map;
+	}
 }
