682749dcfee0348dc65cd85eb3bbc3b04a39c272
Robert Wolfinger <rwolfinger@cellular.de>
2015-10-08 23:49:14 +0200

    Added camera view

--- src/de/dungeonrunner/Application.java
+++ src/de/dungeonrunner/Application.java
@@ -1,6 +1,8 @@
 package de.dungeonrunner;
 
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.View;
+import org.jsfml.system.Vector2f;
 import org.jsfml.window.VideoMode;
 import org.jsfml.window.event.Event;
 
@@ -24,12 +26,26 @@ public class Application {
 		RenderWindow window = new RenderWindow();
 		window.create(new VideoMode(800, 480, 32), "Test");
 		window.setFramerateLimit(30);
+		
+		View view = new View(new Vector2f(window.getSize().x / 2, window.getSize().y / 2), new Vector2f(window.getSize()));
+		window.setView(view);
 
 		while (window.isOpen()) {
 			for (Event event : window.pollEvents()) {
-				if (event.type == Event.Type.CLOSED) {
-					// The user pressed the close button
+				
+				switch(event.type){
+				case  CLOSED:
 					window.close();
+					break;
+				case RESIZED:
+					view= new View(new Vector2f(window.getSize().x / 2, window.getSize().y / 2), new Vector2f(window.getSize()));
+					window.setView(view);
+					break;
+				case KEY_PRESSED:
+					view.move(new Vector2f(10, 0));
+					window.setView(view);
+				default:
+					break;
 				}
 			}
 
--- src/de/dungeonrunner/TmxMapLoader.java
+++ src/de/dungeonrunner/TmxMapLoader.java
@@ -1,6 +1,11 @@
 package de.dungeonrunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import tiled.core.Map;
+import tiled.core.Tile;
+import tiled.core.TileSet;
 import tiled.io.TMXMapReader;
 
 public class TmxMapLoader {
@@ -23,5 +28,22 @@ public class TmxMapLoader {
 
 		System.out.println(map.toString() + " loaded");
 		return map;
+	}
+	
+	public static List<Tile> getBlockingTiles(Map map){
+		List<Tile> blockingTiles = new ArrayList<>();
+		
+		for (int i = 0; i < map.getTileSets().size(); i++) {
+			TileSet tileSet = map.getTileSets().get(i);
+			final int numberTiles = tileSet.size();
+			for (int o = 0; o < numberTiles; o++) {
+				Tile tile = tileSet.getTile(o);
+				boolean isBlockVolume = Boolean.parseBoolean(tile.getProperties().getProperty("BlockVolume", "false"));
+				if(isBlockVolume){
+					blockingTiles.add(tile);
+				}
+			}
+		}
+		return blockingTiles;
 	}
 }
