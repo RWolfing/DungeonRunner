3e9b716d5ab2d38c75555151670f12b28c61f909
Robert Wolfinger <rwolfinger@cellular.de>
2015-12-08 00:30:56 +0100

    Fixed quad tree collision

--- src/de/dungeonrunner/util/QuadTree.java
+++ src/de/dungeonrunner/util/QuadTree.java
@@ -3,152 +3,165 @@ package de.dungeonrunner.util;
 import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.jsfml.graphics.Color;
 import org.jsfml.graphics.FloatRect;
 import org.jsfml.graphics.RectangleShape;
 import org.jsfml.graphics.RenderStates;
 import org.jsfml.graphics.RenderTarget;
+import org.jsfml.graphics.Text;
 import org.jsfml.system.Vector2f;
 
 import de.dungeonrunner.nodes.SceneNode;
+import de.dungeonrunner.singleton.FontHolder;
+import de.dungeonrunner.singleton.FontHolder.FontID;
 
-public class QuadTree extends SceneNode{
+public class QuadTree extends SceneNode {
 
-	private int MAX_OBJECTS = 20;
+	private int MAX_OBJECTS = 10;
 	private int MAX_LEVELS = 5;
-	
+
 	private int mLevel;
-	private List<SceneNode> mObjects;
-	private FloatRect mBounds;
-	private QuadTree[] mNodes;
-	
-	public QuadTree(int pLevel, FloatRect pBounds){
+	private List<SceneNode> mCollisionObjects;
+	private FloatRect mQuadTreeBounds;
+	private QuadTree[] mQuadTreeNodes;
+
+	public QuadTree(int pLevel, FloatRect pBounds) {
 		mLevel = pLevel;
-		mObjects = new ArrayList<>();
-		mBounds = pBounds;
-		mNodes = new QuadTree[4];
+		mCollisionObjects = new ArrayList<>();
+		mQuadTreeBounds = pBounds;
+		mQuadTreeNodes = new QuadTree[4];
 	}
-	
-	public void clear(){
-		mObjects.clear();
-		for(int i = 0; i < mNodes.length; i++){
-			if(mNodes[i] != null) {
-				mNodes[i].clear();
-				mNodes[i] = null;
+
+	public void clear() {
+		mCollisionObjects.clear();
+		for (int i = 0; i < mQuadTreeNodes.length; i++) {
+			if (mQuadTreeNodes[i] != null) {
+				mQuadTreeNodes[i].clear();
+				mQuadTreeNodes[i] = null;
 			}
 		}
 	}
-	
-	private void split(){
-		int subWidth = (int)(mBounds.width / 2);
-		int subHeight = (int)(mBounds.height / 2);
-		int x = (int) mBounds.left;
-		int y = (int) mBounds.top;
-		
-		mNodes[0] = new QuadTree(mLevel + 1, new FloatRect(x + subWidth, y, subWidth, subHeight));
-		mNodes[1] = new QuadTree(mLevel + 1, new FloatRect(x, y, subWidth, subHeight));
-		mNodes[2] = new QuadTree(mLevel + 1, new FloatRect(x, y + subHeight, subWidth, subHeight));
-		mNodes[3] = new QuadTree(mLevel + 1, new FloatRect(x + subWidth, y + subHeight, subWidth, subHeight));
+
+	private void split() {
+		int subWidth = (int) (mQuadTreeBounds.width / 2);
+		int subHeight = (int) (mQuadTreeBounds.height / 2);
+		int x = (int) mQuadTreeBounds.left;
+		int y = (int) mQuadTreeBounds.top;
+
+		mQuadTreeNodes[0] = new QuadTree(mLevel + 1, new FloatRect(x + subWidth, y, subWidth, subHeight));
+		mQuadTreeNodes[1] = new QuadTree(mLevel + 1, new FloatRect(x, y, subWidth, subHeight));
+		mQuadTreeNodes[2] = new QuadTree(mLevel + 1, new FloatRect(x, y + subHeight, subWidth, subHeight));
+		mQuadTreeNodes[3] = new QuadTree(mLevel + 1, new FloatRect(x + subWidth, y + subHeight, subWidth, subHeight));
 	}
-	
-	private int getIndex(FloatRect rect){
+
+	private int getIndex(FloatRect rect) {
 		int index = -1;
-		double verticalMidpoint = mBounds.left + (mBounds.width / 2);
-		double horizontalMidpoint = mBounds.top + (mBounds.height / 2);
-		
-		//TODO
+		double verticalMidpoint = mQuadTreeBounds.left + (mQuadTreeBounds.width / 2);
+		double horizontalMidpoint = mQuadTreeBounds.top + (mQuadTreeBounds.height / 2);
+
 		// Object can completely fit within the top quadrants
-		boolean topQuadrant = (rect.top < horizontalMidpoint && rect.top + rect.height < horizontalMidpoint);
+		boolean topQuadrant = ((rect.top + rect.height) < horizontalMidpoint);
 		// Object can completely fit within the bottom quadrants
 		boolean bottomQuadrant = (rect.top > horizontalMidpoint);
-		
+
 		// Object can completely fit within the left quadrants
-		   if (rect.left < verticalMidpoint && rect.left + rect.width < verticalMidpoint) {
-		      if (topQuadrant) {
-		        index = 1;
-		      }
-		      else if (bottomQuadrant) {
-		        index = 2;
-		      }
-		    }
-		    else if (rect.left >= verticalMidpoint) {
-		     if (topQuadrant) {
-		       index = 0;
-		     }
-		     else if (bottomQuadrant) {
-		       index = 3;
-		     }
-		   }
-		   return index;
+		if ((rect.left + rect.width) < verticalMidpoint) {
+			if (topQuadrant) {
+				index = 1;
+			} else if (bottomQuadrant) {
+				index = 2;
+			}
+		} else if (rect.left > verticalMidpoint) {
+			if (topQuadrant) {
+				index = 0;
+			} else if (bottomQuadrant) {
+				index = 3;
+			}
+		}
+		return index;
 	}
-	
-	public void insert(SceneNode sceneNode){
-		if(sceneNode.getBoundingRect() == null){
+
+	public void insert(SceneNode sceneNode) {
+		if (sceneNode.getBoundingRect() == null) {
 			return;
 		}
-		sceneNode.mColor = Color.WHITE;
-		if(mNodes[0] != null){
-			int index = getIndex(sceneNode.getBoundingRect());
-			
-			if(index != -1){
-				mNodes[index].insert(sceneNode);
-				sceneNode.mColor = Color.YELLOW;
+
+		// Check if we have to put the node in a quadrant
+		int index = getIndex(sceneNode.getBoundingRect());
+		if (index != -1) {
+			if (mQuadTreeNodes[index] != null) {
+				mQuadTreeNodes[index].insert(sceneNode);
 				return;
 			}
 		}
-		
-		mObjects.add(sceneNode);
-		
-		if(mObjects.size() > MAX_OBJECTS && mLevel < MAX_LEVELS){
-			if(mNodes[0] == null){
+
+		// Did not put node in a quadrant
+		mCollisionObjects.add(sceneNode);
+
+		if (mCollisionObjects.size() > MAX_OBJECTS && mLevel < MAX_LEVELS) {
+			if (mQuadTreeNodes[0] == null) {
 				split();
 			}
-			
+
 			int i = 0;
-			while (i < mObjects.size()) {
-				int index = getIndex(mObjects.get(i).getBoundingRect());
-				if(index != -1){
-					mNodes[index].insert(mObjects.remove(i));
+			while (i < mCollisionObjects.size()) {
+				index = getIndex(mCollisionObjects.get(i).getBoundingRect());
+				if (index != -1) {
+					SceneNode node = mCollisionObjects.remove(i);
+					node.mColor = Color.YELLOW;
+					mQuadTreeNodes[index].insert(node);
+
 				} else {
+					mCollisionObjects.get(i).mColor = Color.WHITE;
 					i++;
 				}
 			}
 		}
 	}
-	
-	public List<SceneNode> retrieve(List<SceneNode> returnObjects, FloatRect rect){
-		int index = getIndex(rect);
 
-		if(index != -1 && mNodes[0] != null) {
-			mNodes[index].retrieve(returnObjects, rect);
+	public List<SceneNode> retrieve(List<SceneNode> returnObjects, FloatRect rect) {
+		if((rect.left + rect.width) < mQuadTreeBounds.left || rect.left > (mQuadTreeBounds.left +  mQuadTreeBounds.width) || (rect.top + rect.height) < mQuadTreeBounds.top || rect.top > (mQuadTreeBounds.top + mQuadTreeBounds.height)){
+			System.err.println("Collision rectangle not in bounds of the quadtree!");
+			return returnObjects;
 		}
 		
-		returnObjects.addAll(mObjects);
+		int index = getIndex(rect);
+		if (index != -1 && mQuadTreeNodes[0] != null) {
+			mQuadTreeNodes[index].retrieve(returnObjects, rect);
+		} else {
+			//The rect did not fit in any quadtree, return all containing nodes as collisions
+			for (int i = 0; i < mQuadTreeNodes.length; i++) {
+				QuadTree node = mQuadTreeNodes[i];
+				if (node != null) {
+					returnObjects.addAll(node.mCollisionObjects);
+				}
+			}
+		}
+		returnObjects.addAll(mCollisionObjects);
 		return returnObjects;
 	}
 
 	@Override
 	public FloatRect getBoundingRect() {
-		return new FloatRect(mBounds.left, mBounds.top, mBounds.width, mBounds.height);
+		return new FloatRect(mQuadTreeBounds.left, mQuadTreeBounds.top, mQuadTreeBounds.width, mQuadTreeBounds.height);
 	}
 
 	@Override
 	public void draw(RenderTarget target, RenderStates states) {
 		RectangleShape shape = new RectangleShape();
-		shape.setPosition(new Vector2f(mBounds.left, mBounds.top));
-		shape.setSize(new Vector2f(mBounds.width, mBounds.height));
+		shape.setPosition(new Vector2f(mQuadTreeBounds.left, mQuadTreeBounds.top));
+		shape.setSize(new Vector2f(mQuadTreeBounds.width, mQuadTreeBounds.height));
 		shape.setFillColor(Color.TRANSPARENT);
 		shape.setOutlineColor(Color.RED);
 		shape.setOutlineThickness(1.0f);
 		target.draw(shape);
-		
-		for(int i = 0; i < mNodes.length; i++){
-			if(mNodes[i] != null){
-				mNodes[i].draw(target, states);
+
+		for (int i = 0; i < mQuadTreeNodes.length; i++) {
+			if (mQuadTreeNodes[i] != null) {
+				mQuadTreeNodes[i].draw(target, states);
 			}
 		}
 	}
-	
-	
 }
