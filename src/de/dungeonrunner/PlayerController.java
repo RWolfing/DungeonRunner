0359b6f4f010d7f77d1d194f89bf5a4862bc4e34
Robert Wolfinger <rwolfinger@cellular.de>
2015-12-11 20:33:21 +0100

    Addes movement and jump to player

--- src/de/dungeonrunner/PlayerController.java
+++ src/de/dungeonrunner/PlayerController.java
@@ -3,11 +3,23 @@ package de.dungeonrunner;
 import org.jsfml.window.Keyboard;
 import org.jsfml.window.Keyboard.Key;
 import org.jsfml.window.event.Event;
+import org.jsfml.window.event.Event.Type;
 
 public class PlayerController {
 
 	public void handleEvent(Event event, CommandStack commands) {
-		// do nothing yet
+
+		//Key Pressed Event
+		if(event.type == Type.KEY_PRESSED){
+			switch(event.asKeyEvent().key){
+			case UP:
+				JumpCommand jumpCommand = new JumpCommand(NodeType.PLAYER);
+				commands.push(jumpCommand);
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 	public void handleRealtimeInput(CommandStack commands) {
@@ -15,12 +27,12 @@ public class PlayerController {
 		boolean isRightKeyPressed = Keyboard.isKeyPressed(Key.RIGHT);
 
 		if (isLeftPressed) {
-			MoveCommand moveCommand = new MoveCommand(NodeType.PLAYER, -5, 0);
+			MoveCommand moveCommand = new MoveCommand(NodeType.PLAYER, -2, 0);
 			commands.push(moveCommand);
 		}
 		
 		if(isRightKeyPressed) {
-			MoveCommand moveCommand = new MoveCommand(NodeType.PLAYER, 5, 0);
+			MoveCommand moveCommand = new MoveCommand(NodeType.PLAYER, 2, 0);
 			commands.push(moveCommand);
 		}
 	}
